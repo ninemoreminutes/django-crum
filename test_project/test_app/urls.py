5d56341894f01657cd58f6bfc7500c0632885bc2
Chris Church <chris@ninemoreminutes.com>
2020-11-03 14:53:04 -0500

    Update requirements, add Python 3.9 testing/support, fix test failure with Python 3.5 + Django 1.11.

--- test_project/test_app/urls.py
+++ test_project/test_app/urls.py
@@ -1,5 +1,8 @@
 # Django
-from django.conf.urls import url
+try:
+    from django.urls import re_path
+except ImportError:
+    from django.conf.urls import url as re_path
 
 # Test App
 from test_project.test_app.views import index, api_index
@@ -8,6 +11,6 @@ from test_project.test_app.views import index, api_index
 app_name = 'test_app'
 
 urlpatterns = [
-    url(r'^$', index, name='index'),
-    url(r'^api/$', api_index, name='api_index'),
+    re_path(r'^$', index, name='index'),
+    re_path(r'^api/$', api_index, name='api_index'),
 ]
--- test_project/urls.py
+++ test_project/urls.py
@@ -1,18 +1,21 @@
 # Django
 from django.conf import settings
-from django.conf.urls import include, url
+try:
+    from django.urls import include, re_path
+except ImportError:
+    from django.conf.urls import include, url as re_path
 
 include_kwargs = dict(namespace='test_app')
 
 urlpatterns = [
-    url(r'^test_app/', include('test_project.test_app.urls', **include_kwargs)),
+    re_path(r'^test_app/', include('test_project.test_app.urls', **include_kwargs)),
 ]
 
 if 'django.contrib.admin' in settings.INSTALLED_APPS:
     from django.contrib import admin
     admin.autodiscover()
     urlpatterns += [
-        url(r'', admin.site.urls),
+        re_path(r'', admin.site.urls),
     ]
 
 if 'django.contrib.staticfiles' in settings.INSTALLED_APPS and settings.DEBUG:
